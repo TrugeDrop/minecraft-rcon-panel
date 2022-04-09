afab3f67b2c9cbf8f3963151ef19b8d709e3f432
TrugeDrop <95874423+TrugeDrop@users.noreply.github.com>
2022-04-09 09:28:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbURL: "mongodb+srv://PVCO:14-4_V@cluster0.kbfyq.mongodb.net/AoonramelMinecraft?retryWrites=true&w=majority",
+  dbURL: "MongoDB URL",
   secret: "secret",
   port: 2912,
   admin: "2022"
